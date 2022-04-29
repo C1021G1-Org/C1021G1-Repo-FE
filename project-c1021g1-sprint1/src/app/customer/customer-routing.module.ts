1aed586777da048a794d0ed104402cf7d8dcdc29
ductinh0402 <hoangductinh0402@gmail.com>
2022-04-29 14:07:37 +0700

    tinh pull

--- project-c1021g1-sprint1/src/app/customer/customer-routing.module.ts
+++ project-c1021g1-sprint1/src/app/customer/customer-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListCustomerComponent} from "./list-customer/list-customer.component";
+import {CreateCustomerComponent} from "./create-customer/create-customer.component";
+import {DeleteCustomerComponent} from "./delete-customer/delete-customer.component";
+import {EditCustomerComponent} from "./edit-customer/edit-customer.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list-customer', component: ListCustomerComponent
+  },
+  {
+    path: 'create-customer', component: CreateCustomerComponent
+  },
+  {
+    path: 'delete-customer/:id', component: DeleteCustomerComponent
+  },
+  {
+    path: 'edit-customer/:id', component: EditCustomerComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
