56fff88ab8d65918e456b4a3f9acc29da7576e1d
thuan <you@example.com>
2022-04-29 17:02:46 +0700

    thuanpd

--- project-c1021g1-sprint1/src/app/report/report.service.ts
+++ project-c1021g1-sprint1/src/app/report/report.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+  URL_REPORT = 'http://localhost:8080/report-price';
+  URL_REPORT_EMPLOYEE = 'http://localhost:8080/report-employee';
 
-  constructor() { }
+  constructor(private  httpClient: HttpClient) {
+  }
+
+
+  getAllReport(): Observable<any> {
+    return this.httpClient.get<any>(this.URL_REPORT);
+  }
+
+  getAllReportEmployee(): Observable<any> {
+    return this.httpClient.get<any>(this.URL_REPORT_EMPLOYEE);
+  }
 }
--- project-c1021g1-sprint1/src/app/report/statistic/statistic.component.ts
+++ project-c1021g1-sprint1/src/app/report/statistic/statistic.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ReportServiceService} from "../service/report-service.service";
 import * as Highcharts from "highcharts";
+import {ReportService} from "../report.service";
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.component.html',
@@ -10,13 +10,17 @@ export class StatisticComponent implements OnInit {
   select1 = '';
   select2 = '';
   private option : any;
+  private chart: any;
+  selectShape: any;
+  nameEmployee: any;
 
   startMonth: number;
   endMonth: number;
 
-  constructor(private reportServiceService: ReportServiceService) { }
+  constructor(private reportServiceService: ReportService) { }
 
   ngOnInit(): void {
+
   }
 
 
@@ -35,61 +39,114 @@ export class StatisticComponent implements OnInit {
     console.log("enđat" + this.endMonth);
     const currentDate = new Date();
 
-    this.reportServiceService.getAllReport().subscribe(data => {
-      for (let i = 0; i < data.length; i++) {
-        switch (Number(data[i].monthStartDate)) {
-          case this.startMonth:
-            this.startMonth = Number(data[i].totalPrice);
-            break;
-          case this.endMonth:
-            this.endMonth = Number(data[i].totalPrice);
-            break;
+   if (this.selectShape == 1){
+     this.reportServiceService.getAllReport().subscribe(data => {
+       for (let i = 0; i < data.length; i++) {
+         switch (Number(data[i].monthStartDate)) {
+           case this.startMonth:
+             this.startMonth = Number(data[i].totalPrice);
+             break;
+           case this.endMonth:
+             this.endMonth = Number(data[i].totalPrice);
+             break;
+         }
+       }
+       this.option = {
+         chart: {
+           plotBackgroundColor: null,
+           plotBorderWidth: null,
+           plotShadow: false,
+           type: 'pie'
+         },
+         title: {
+           text: 'Danh thu vé máy bay tháng ' + (currentDate.getMonth() + 1)
+         },
+         tooltip: {
+           pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+         },
+         accessibility: {
+           point: {
+             valueSuffix: '%'
+           }
+         },
+         plotOptions: {
+           pie: {
+             allowPointSelect: true,
+             cursor: 'pointer',
+             dataLabels: {
+               enabled: true,
+               format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+             }
+           }
+         },
+         series: [{
+           name: 'Brands',
+           colorByPoint: true,
+           data: [
+             {
+               name: 'Tháng' + this.select1,
+               y: this.startMonth
+             }
+             , {
+               name: 'Tháng' + this.select2,
+               y: this.endMonth
+             }
+           ]
+         }]
+       };
+       Highcharts.chart('container', this.option);
+     });
+   }else{
+    this.reportServiceService.getAllReportEmployee().subscribe(value =>{
+      console.log(value +"dsgdg")
+      for (let i = 0; i < value.length; i++){
+        if (Number(value[i].monthEmployee)){
+          this.startMonth = Number(value[i].sumPoint)
+          this.nameEmployee = String(value[i].name_employee)
+          console.log(this.startMonth);
+          console.log(this.nameEmployee);
+          this.chart = {
+            chart: {
+              type: 'bar'
+            },
+            title: {
+              text: 'Stacked bar chart'
+            },
+            // xAxis: {
+            //   categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+            // },
+            yAxis: {
+              min: 0,
+              title: {
+                text: 'Total fruit consumption'
+              }
+            },
+            legend: {
+              reversed: true
+            },
+            plotOptions: {
+              series: {
+                stacking: 'normal'
+              }
+            },
+            series: [
+              {
+                name: [this.nameEmployee[0],this.nameEmployee[1]],
+                data: [this.startMonth[0],this.startMonth[1]]
+              }
+            ]
+          };
+
+
         }
       }
-      this.option = {
-        chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false,
-          type: 'pie'
-        },
-        title: {
-          text: 'Danh thu vé máy bay tháng ' + (currentDate.getMonth() + 1)
-        },
-        tooltip: {
-          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        accessibility: {
-          point: {
-            valueSuffix: '%'
-          }
-        },
-        plotOptions: {
-          pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-              enabled: true,
-              format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            }
-          }
-        },
-        series: [{
-          name: 'Brands',
-          colorByPoint: true,
-          data: [
-            {
-              name: 'Tháng' + this.select1,
-              y: this.startMonth
-            }
-            , {
-              name: 'Tháng' + this.select2,
-              y: this.endMonth
-            }
-          ]
-        }]
-      };
-      Highcharts.chart('container', this.option);
-    });
+
+      Highcharts.chart('chart',this.chart);
+    })
+
+   }
+
+
   }
+
 }
