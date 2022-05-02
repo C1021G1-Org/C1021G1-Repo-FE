7db00a6cb9bf0677d1d3aa5b3b6607c51a2d9947
Ho Nhat Hieu <honhathieu97@gmail.com>
2022-05-02 16:24:02 +0700

    push

---
+++ project-c1021g1-sprint1/src/app/flight/model/airline-type.ts
@@ -0,0 +1,7 @@
+export interface AirlineType {
+  id: number;
+  nameFlight: string;
+  priceFlight: string;
+  imageAirline: string;
+  delFlagAirline: boolean;
+}
---
+++ project-c1021g1-sprint1/src/app/flight/model/flight.ts
@@ -0,0 +1,12 @@
+import {AirlineType} from "./airline-type";
+
+export interface Flight {
+  id : number;
+  codeFlight: string;
+  fromFlight: string;
+  toFlight: string;
+  dateStart: string;
+  dateEnd: string;
+  delFlagFlight: boolean;
+  airlineType: AirlineType
+}
